987c26a0bc2b6dc46e68fb740229cafb8746744b
Stefano Soro <stefano@safetech.inf.br>
2020-06-18 13:58:44 -0300

    :sparkles: Updating new lessons

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,45 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Blue', 'Black', 'Red', 'Green']
+      'answers': [
+        {'text': 'Blue', 'score': 6},
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 8},
+        {'text': 'Green', 'score': 0}
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Snake', 'Lion', 'Turtle', 'Cat']
+      'answers': [
+        {'text': 'Snake', 'score': 10},
+        {'text': 'Lion', 'score': 4},
+        {'text': 'Turtle', 'score': 3},
+        {'text': 'Cat', 'score': 2},
+      ]
     },
     {
       'questionText': 'What\'s your favorite instructor?',
-      'answers': ['Snake', 'Max', 'Diego', 'Felipe']
+      'answers': [
+        {'text': 'Snape', 'score': 10},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Diego', 'score': 4},
+        {'text': 'Felipe', 'score': 2},
+      ]
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -58,7 +83,7 @@ class _MyAppState extends State<MyApp> {
                   questions: _questions,
                   questionIndex: _questionIndex,
                 )
-              : Result()),
+              : Result(_totalScore, _resetQuiz)),
     );
   }
 }
